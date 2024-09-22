1d1d0860edc52dbe0cf600e6332bc47ee9bb0ce4
hanwoosung1 <110220484+hanwoosung1@users.noreply.github.com>
2024-09-22 15:26:18 +0900

    테스트

---
+++ src/test/java/org/example/borad_test/BoradTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.borad_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoradTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
